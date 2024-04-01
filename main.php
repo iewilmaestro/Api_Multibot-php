06218a46b4f4d6b3fa5254d7ae16d0ac3665fbdc
iewilmaestro <83399918+iewilmaestro@users.noreply.github.com>
2024-04-01 17:41:14 +0700

    Update main.php

--- main.php
+++ main.php
@@ -1,9 +1,11 @@
 <?php
 Class RequestApi{
-	function in_api($data, $method = "POST"){
-		$data =  "key=".$this->apikey."&json=1&".$data;
-		if($method == "GET")return json_decode(file_get_contents($this->host.'/in.php?'.$data),1);
-		$opts = ['http' =>['method'  => 'POST','content' => $data]];
+	function in_api($content, $method, $header = 0){
+		$param = "key=".$this->apikey."&json=1&".$content;
+		if($method == "GET")return json_decode(file_get_contents($this->host.'/in.php?'.$param),1);
+		$opts['http']['method'] = $method;
+		if($header) $opts['http']['header'] = $header;
+		$opts['http']['content'] = $param;
 		return json_decode(file_get_contents($this->host.'/in.php', false, stream_context_create($opts)),1);
 	}
 	function res_api($api_id){
@@ -25,8 +27,8 @@ Class RequestApi{
 			$a++;
 		}
 	}
-	function getResult($data ,$method = 0){
-		$get_in = $this->in_api($data ,$method);
+	function getResult($data ,$method, $header = 0){
+		$get_in = $this->in_api($data ,$method, $header);
 		if(!$get_in["status"]){
 			print $get_in["request"]."\n";
 			return 0;
@@ -49,56 +51,70 @@ Class ApiMultibot extends RequestApi {
 	
 	function __construct($apikey){
 		$this->host = "http://api.multibot.in";
+		$this->provider = "multibot";
 		$this->apikey = $apikey;
 	}
 	function RecaptchaV2($sitekey, $pageurl){
-		$data = "method=userrecaptcha&sitekey=$sitekey&pageurl=$pageurl";
-		return $this->getResult($data);
+		$data = http_build_query([
+			"method" => "userrecaptcha",
+			"sitekey" => $sitekey,
+			"pageurl" => $pageurl
+			]);
+		return $this->getResult($data, "GET");
 	}
 	function Hcaptcha($sitekey, $pageurl ){
-		$data = "method=hcaptcha&sitekey=$sitekey&pageurl=$pageurl";
-		return $this->getResult($data);
+		$data = http_build_query([
+			"method" => "hcaptcha",
+			"sitekey" => $sitekey,
+			"pageurl" => $pageurl
+			]);
+		return $this->getResult($data, "GET");
 	}
 	function Turnstile($sitekey, $pageurl){
-		$data = "method=turnstile&sitekey=".$sitekey."&pageurl=".$pageurl;
+		$data = http_build_query([
+			"method" => "turnstile",
+			"sitekey" => $sitekey,
+			"pageurl" => $pageurl
+			]);
 		return $this->getResult($data, "GET");
 	}
 	function Ocr($img){
-		$data = "method=universal&body=".trim(str_replace('data:image/png;base64,','',$img));
-		return $this->getResult($data);
+		$data = http_build_query([
+			"method" => "universal",
+			"body" => $img
+			]);
+		return $this->getResult($data, "POST");
 	}
 	function AntiBot($source){
-		$main = explode('"',explode('<img src="',explode('Bot links',$source)[1])[1])[0];
+		/*
+		# true Data like this, but i make easy with source website
+		$data = http_build_query([
+			"method" => "antibot",
+			"main" => "data:image/png;base64,iVxxxx",
+			"6181" => "data:image/png;base64,iVxxxx",
+			"1354" => "data:image/png;base64,iVxxxx",
+			"5643" => "data:image/png;base64,iVxxxx"
+			]);
+		*/
+		$main = explode('"',explode('src="',explode('Bot links',$source)[1])[1])[0];
 		if(!$main)return 0;
-		$antiBot["main"] = $main;
+		$data["method"] = "antibot";
+		$data["main"] = $main;
 		$src = explode('rel=\"',$source);
 		foreach($src as $x => $sour){
 			if($x == 0)continue;
 			$no = explode('\"',$sour)[0];
 			$img = explode('\"',explode('src=\"',$sour)[1])[0];
-			$antiBot[$no] = $img;
+			$data[$no] = $img;
 		}
+		$data = http_build_query($data);
+		//print_r($data);
 		$ua = "Content-type: application/x-www-form-urlencoded";
-		$data = ["key"=>$this->apikey,"method"=>"antibot","json"=>1] + $antiBot;
-		$opts = ['http' =>['method'  => 'POST','header' => $ua,'content' => http_build_query($data)]];
-		$get_in = json_decode(file_get_contents($this->host.'/in.php', false, stream_context_create($opts)),1);
-		if(!$get_in["status"]){
-			print $get_in["request"]."\n";
-			return 0;
-		}
-		while(true){
-			echo " bypass |   \r";
-			$get_res = $this->res_api($get_in["request"]);
-			if($get_res["request"] == "CAPCHA_NOT_READY"){
-				echo " bypass ─ \r";
-				$this->wait(10);
-				continue;
-			}
-			if($get_res["status"])return "+".str_replace(",","+",$get_res['request']);
-			return 0;
-		}
+		$res = $this->getResult($data, "POST", $ua);
+		return "+".str_replace(",","+",$res);
 	}
 }
+
 error_reporting(0);
 # because no headers file get contents
 
@@ -115,36 +131,36 @@ print " Balance: ".$balance."\n";;
 $sitekey = "6LfD3PIbAAAAAJs_eEHvoOl75_83eXSqpPSRFJ_u";
 $pageurl = "https://2captcha.com/demo/recaptcha-v2";
 $reCaptcha = $api->RecaptchaV2($sitekey, $pageurl );
-print " reCaptcha: ".$reCaptcha."\n";
-# 03AFcWeA5dAXT8iT12IArrMsKLGrL2qgcGhPp2ES7BWgtPIa5GxGXorB
+print " reCaptcha: ".substr($reCaptcha,0,20)."\n";
+# 03AFcWeA4Rup5qQLKz3O
 
 # hCaptcha 0.0055
 $sitekey = "9409f20b-6b75-4057-95c4-138e85f69789";
 $pageurl = "https://2captcha.com/demo/hcaptcha?difficulty=always-on";
 $hCaptcha =  $api->Hcaptcha($sitekey, $pageurl );
-print " hCaptcha: ".$hCaptcha."\n";
-// P1_eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.hadwYXNza2V5xQWA4
+print " hCaptcha: ".substr($hCaptcha,0,20)."\n";
+// P1_eyJ0eXAiOiJKV1QiL
 
 
 # turnstile
 $pageurl = "https://onlyfaucet.com/faucet/currency/ltc";
 $sitekey = "0x4AAAAAAAPSP6CaBc510-qc";
 $Turnstile = $api->Turnstile($sitekey, $pageurl);
-print " turstile: ".$Turnstile."\n";
-# 0.5YsJy3i-JlJ7QYJnEVXlf6SH83xu7W125CFG060y
+print " turstile: ".substr($Turnstile,0,20)."\n";
+# 0.8IlRqCONhotKoKHZFk
 
 
 # image Ocr
 # image as base64
-# Example
 $img = base64_encode(file_get_contents("https://nopecha.com/image/demo/textcaptcha/00Ge55.png"));
 $Ocr = $api->Ocr($img);
 print " ocr: ".$Ocr."\n";
 # o0ge55
 
 
+
 # anti-botlinks 
 $source = file_get_contents("https://bitonefaucet.com.tr/rsshort/index.php");
 $Antibot = $api->AntiBot($source);
 print " antibotlink: ".$Antibot."\n";
-# +1905+1004+8392+10241004+8392+1024
+# +6378+7470+8895+5907
